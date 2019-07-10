e95409263ade611578def43ae305a256b4722f11
ely-uf <elyahove@student.unit.ua>
2019-07-10 21:58:43 +0300

    Add a define for the number of philosophers.

--- philo_const.h
+++ philo_const.h
@@ -1,6 +1,14 @@
 #ifndef __PHILO_CONST_H
 # define __PHILO_CONST_H
 
+# ifndef PHILO_N
+#  define PHILO_N	7
+# endif
+
+# if PHILO_N % 2 == 0
+#  error "The number of philosophers should be odd."
+# endif
+
 # define MAX_LIFE   10
 # define EAT_T      1
 # define REST_T     3
