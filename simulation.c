d5ec1b16ec4c6852d1094e676798e5dd51d5c89a
ely-uf <elyahove@student.unit.ua>
2019-07-17 21:46:28 +0300

    Add philosopher run/stop and time tracking for simulation.

--- simulation.c
+++ simulation.c
@@ -1,7 +1,10 @@
+#include <sys/time.h>
 #include "simulation.h"
 #include "philo_const.h"
 #include "philosopher.h"
 #include "fork.h"
+#include "event.h"
+#include "eventq_singleton.h"
 
 static int	simulation_init(t_philosopher ph[PHILO_N], t_fork forks[FORK_N])
 {
@@ -21,12 +24,16 @@ static int	simulation_init(t_philosopher ph[PHILO_N], t_fork forks[FORK_N])
 		i++;
 	}
 
+	event_dispatch(eventq_get(), (t_event){ .type = P_SIMULATION_INIT });
+
 	return (0);
 }
 
 static void	simulation_deinit(t_philosopher ph[PHILO_N], t_fork forks[FORK_N])
 {
 	size_t	i;
+
+	event_dispatch(eventq_get(), (t_event){ .type = P_SIMULATION_END });
 
 	i = 0;
 	while (i < FORK_N)
@@ -37,16 +44,48 @@ static void	simulation_deinit(t_philosopher ph[PHILO_N], t_fork forks[FORK_N])
 	(void)ph;
 }
 
+static void	philosophers_run(t_philosopher philosophers[PHILO_N])
+{
+	size_t	i;
+
+	i = 0;
+	while (i < PHILO_N)
+	{
+		philosopher_run(&philosophers[i]);
+		i++;
+	}
+}
+
+static void	philosophers_stop(t_philosopher philosophers[PHILO_N])
+{
+	size_t	i;
+
+	i = 0;
+	while (i < PHILO_N)
+	{
+		philosopher_stop(&philosophers[i]);
+		i++;
+	}
+}
+
 int			simulation_run(void)
 {
 	t_philosopher	philosophers[PHILO_N];
 	t_fork			forks[FORK_N];
+	time_t			start_time;
 
 	simulation_init(philosophers, forks);
+	start_time = time(NULL);
 
-	/*
-	 *	TODO: Run philosopher threads.
-	 */
+	philosophers_run(philosophers);
+
+	while (simulation_alive())
+	{
+		if (time(NULL) - start_time >= TIMEOUT)
+			simulation_stop();
+	}
+
+	philosophers_stop(philosophers);
 
 	simulation_deinit(philosophers, forks);
 
